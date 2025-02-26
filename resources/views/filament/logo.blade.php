725a68fa9264342e26bfcc141d6d3245470c4057
Janany67 <jananyvenkatesh@gmail.com>
2025-02-26 10:45:40 +0530

    Admin panel logo fixed

--- resources/views/filament/logo.blade.php
+++ resources/views/filament/logo.blade.php
@@ -1,2 +1,7 @@
-<img src="/logo/pngtree-wolf-logo-png-image_2306634.jpg" alt="" class="h-12 w-auto" 
+@php
+    $brand = \App\Models\Logo::first(); // Fetch the first logo entry
+@endphp
+@if ($brand && $brand->brand)
+<img src="{{ asset('storage/' . $brand->brand) }}" alt="" class="h-12 w-auto"
     style="width: 100px; height: 59px; margin-top: -17px;"/>
+@endif
